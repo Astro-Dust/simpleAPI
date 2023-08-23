bc24162b39d85da7c1af8135a3a4768cb664b0f9
Astro-Dust <lucaslucenagdrive@gmail.com>
2023-08-22 23:30:21 -0300

    Projeto simples usando Spring Boot que retorna dados em formato JSON

---
+++ src/main/java/com/moonlit/simpleAPI/entities/Product.java
@@ -0,0 +1,49 @@
+package com.moonlit.simpleAPI.entities;
+
+public class Product {
+
+	private Long id;
+	private String name;
+	private Double price;
+	
+	// no exercicio é pedido para organizar cada produto de acordo com seu departamento
+	private Department department;
+	
+	public Product() {
+		
+	}	
+	
+	public Product(Long id, String name, Double price, Department department) {
+		this.id = id;
+		this.name = name;
+		this.price = price;
+		this.department = department;
+	}
+
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Double getPrice() {
+		return price;
+	}
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	public Department getDepartment() {
+		return department;
+	}
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+	
+	
+}
